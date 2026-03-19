504280ad654dca171771aaa62332245c62491490
kori-code <susilsfriends10@gmail.com>
2026-03-19 15:13:30 +0000

    Fixed missing firebase congig in production environment

--- sabuja-app/src/environments/environment.prod.ts
+++ sabuja-app/src/environments/environment.prod.ts
@@ -1,3 +1,11 @@
 export const environment = {
-  production: true
+  production: true,
+  firebase: {
+    apiKey: "YOUR_FIREBASE_API_KEY",
+    authDomain: "brahmapur-sabuja-bahini.firebaseapp.com",
+    projectId: "brahmapur-sabuja-bahini",
+    storageBucket: "brahmapur-sabuja-bahini.appspot.com",
+    messagingSenderId: "YOUR_SENDER_ID",
+    appId: "YOUR_APP_ID"
+  }
 };
--- sabuja-app/src/environments/environment.ts
+++ sabuja-app/src/environments/environment.ts
@@ -1,11 +1,11 @@
 export const environment = {
   production: false,
   firebase: {
-    apiKey: "YOUR_KEY_HERE",
+    apiKey: "YOUR_API_KEY",
     authDomain: "brahmapur-sabuja-bahini.firebaseapp.com",
     projectId: "brahmapur-sabuja-bahini",
     storageBucket: "brahmapur-sabuja-bahini.appspot.com",
-    messagingSenderId: "YOUR_ID",
+    messagingSenderId: "YOUR_SENDER_ID",
     appId: "YOUR_APP_ID"
   }
 };
