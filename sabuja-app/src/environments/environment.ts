d705d7a37590cdba5cc9354128b35d5b04d0d97e
kori-code <susilsfriends10@gmail.com>
2026-03-19 14:58:22 +0000

    Installed firebase and fixed environment config

--- sabuja-app/src/environments/environment.ts
+++ sabuja-app/src/environments/environment.ts
@@ -1,11 +1,11 @@
 export const environment = {
   production: false,
   firebase: {
-    apiKey: "YOUR_API_KEY",
-    authDomain: "your-project.firebaseapp.com",
-    projectId: "your-project-id",
-    storageBucket: "your-project.appspot.com",
-    messagingSenderId: "123456789",
-    appId: "1:123456789:web:abcdef123"
+    apiKey: "YOUR_KEY_HERE",
+    authDomain: "brahmapur-sabuja-bahini.firebaseapp.com",
+    projectId: "brahmapur-sabuja-bahini",
+    storageBucket: "brahmapur-sabuja-bahini.appspot.com",
+    messagingSenderId: "YOUR_ID",
+    appId: "YOUR_APP_ID"
   }
 };
