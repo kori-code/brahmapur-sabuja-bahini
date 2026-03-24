532d63b61a714ade29b2a775b3467ffa3f947517
kori-code <susilsfriends10@gmail.com>
2026-03-24 20:21:02 +0000

    changed page redirection for superadmin users

--- sabuja-app/src/app/login/login.page.ts
+++ sabuja-app/src/app/login/login.page.ts
@@ -12,6 +12,8 @@ export class LoginPage {
   email: string = '';
   password: string = '';
 
+  private readonly superAdminUser='susilsfriends10@gmail.com';
+
   constructor(
     @Inject(AngularFireAuth) private afAuth: AngularFireAuth,
     private router: Router
@@ -20,7 +22,11 @@ export class LoginPage {
   async login() {
     try {
       await this.afAuth.signInWithEmailAndPassword(this.email, this.password);
-      this.router.navigate(['/tabs/tab1']);
+      if(this.email === this.superAdminUser){
+        this.router.navigate(['/admin']);
+      } else {
+        this.router.navigate(['/tabs/tab1']);
+      }
     } catch (error: any) {
       alert('Login Error: ' + error.message);
     }
